755cdc6610e2c7e032acbb6c7ad2fee8cd43e2ce
fcihpy <fcihpy@163.com>
2015-07-16 18:51:17 +0800

    init
    0716

---
+++ NetworkProccessDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetworkProccessDemo
+//
+//  Created by zhisheshe on 15/7/16.
+//  Copyright (c) 2015年 fcihpy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios,'7.0'
+
+pod 'AFNetworking', '~> 2.5.3'
+pod 'SDWebImage', '~> 3.7.2'
+pod 'MJRefresh', '~> 1.4.6'
+pod 'MBProgressHUD', '~> 0.9.1'
+pod 'Reachability',  '~> 3.0.0'
